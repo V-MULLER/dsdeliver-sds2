d6aef4682f9b7655a79e274c71c4d782fad48eef
Vinicio Muller <vinicio.muller@outlook.com>
2021-01-07 18:01:29 -0300

    Create front-web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import './App.css';
+
+
+function App() {
+
+  useEffect(() => {
+    //chamada para a API para buscar produtos
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
