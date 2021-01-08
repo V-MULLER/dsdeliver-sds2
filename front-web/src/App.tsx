58b65198bda04f4f1b5f7f1b9cf645182c5ef4f6
Vinicio Muller <vinicio.muller@outlook.com>
2021-01-08 00:08:32 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,20 +1,19 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import Navbar from './Navbar';
-
+import Home from './Home'
 
 function App() {
-
+  
   useEffect(() => {
     //chamada para a API para buscar produtos
   }, []);
 
   return (
-    <div className="App">
-      <div>
-        <Navbar />
-      </div>
-    </div>
+    <>
+      <Navbar />
+      <Home />   
+    </>
   );
 }
 
